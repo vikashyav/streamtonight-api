f021a0bd5c906e56e8e29dfeb3bc0faba54bd379
vikashyav <142897671+vikashyav@users.noreply.github.com>
2023-09-05 18:45:40 +0000

    some modification for vercel build

---
+++ .scripts/webpack.config.js
@@ -0,0 +1,47 @@
+const webpack = require("webpack");
+const path = require("path");
+const copy = require("copy-webpack-plugin");
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = () => {
+	return {
+		mode: "production",
+		entry: ["babel-polyfill", path.join(__dirname, "../app.js")],
+		output: {
+			path: path.resolve(__dirname, `../dist`),
+			publicPath: "/",
+			filename: `day2movies-api.js`,
+		},
+		target: "node",
+		node: {
+			// Need this when working with express, otherwise the build fails
+			__dirname: false, // if you don't put this is, __dirname
+			__filename: false, // and __filename return blank or /
+		},
+		externals: [nodeExternals()], // Need this to avoid error when working with Express
+		module: {
+			rules: [
+				{
+					test: /\.jsx?$/,
+					exclude: /node_modules/,
+					loaders: ["babel-loader"],
+				},
+			],
+		},
+		plugins: [
+			new webpack.ProgressPlugin(),
+			new webpack.LoaderOptionsPlugin({
+				minimize: true,
+				debug: false,
+				noInfo: false,
+			}),
+			new copy([
+				{
+					from: `../.env`,
+					to: `../dist/.env`,
+					toType: "file",
+				}
+			]),
+		],
+	};
+};
