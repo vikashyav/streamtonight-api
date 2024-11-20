869fd56727735fc900f6d3e5715f731399a322b6
vikas kumar <vy24754@gmail.com>
2024-11-21 02:21:09 +0530

    added msg

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ import moviesRoutes from "./movies";
 const router = express.Router();
 
 router.get("/", ((req, res) => {
-	const data = "This is base {/index} API. Use the respective routing-API to begin.";
+	const data = "This is base {/index} API. Use the respective routing-API to begin...";
 	return res.status(200).send(data);
 }));
 router.use("/movie", moviesRoutes);
